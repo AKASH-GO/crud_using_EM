3764d76e03fca0f0bdfb1adf1b7a29a3246a9706
AKASH <88474098+AKASH-GO@users.noreply.github.com>
2023-10-19 15:55:40 +0530

    api created

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -31,21 +31,70 @@ exports.create = function (req, res){
 
 // retrieve and return all users / retrieve and return a single user
 exports.find = function (req, res){
-    Userdb.find()
+
+    if(req.query.id){
+        const id = req.query.id;
+        Userdb.findById(id)
+            .then(data=>{
+                if(!data){
+                    res.status(404).send({message:"Not found user with id "+ id})
+                }else{
+                    res.send(data)
+                }
+            })
+            .catch(err=>{
+                res.status(500).send({message:"Error retrieving user with id "+ id})
+            })
+    }
+    else
+    {
+        Userdb.find()
         .then(user=>{
             res.send(user)
         })
         .catch(err=>{
             res.status(500).send({message:err.message || "Error Occurred while retriving user information"})
         })
+    }
 }
 
 // Update a new identified user by user id
 exports.update = function (req, res){
-
+    if (!req.body){
+        return res
+            .status(400)
+            .send({message: "Data to update can not be empty"})
+    }
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id,req.body,{useFindAndModify:false})
+        .then(data=>{
+            if(!data){
+                res.status(404).send({message:`Cannot Update user with ${id}. Maybe user not found!`})
+            }else{
+                res.send(data)
+            }
+        })
+        .catch(err=>{
+            res.status(500).send({message:"Error Update user information"})
+        })
 }
 
 // Delete a user with specified user id in the request
 exports.delete = function (req, res){
-
+    const id = req.params.id;
+    Userdb.findByIdAndDelete(id)
+        .then(data=>{
+            if(!data){
+                res.status(404).send({message:`Cannot Delete with id ${id}. Maybe id is wrong`})
+            }else{
+                res.send({
+                    message:"User was deleted successfully!"
+                })
+            }
+        })
+        .catch(err=>{
+            res.status(500).send({
+                message:"Could not delete User with id=" + id
+            });
+        });
 }
